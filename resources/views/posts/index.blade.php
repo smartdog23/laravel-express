bf5700a619a9f05ff2d556a790f100d30c126776
Valdir Lourenco <valdir2310@yahoo.com.br>
2015-07-11 12:19:10 +0000

    comments no template

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,11 @@
     @foreach($posts as $post)
         <h2>{{ $post->title }}</h2>
         <p>{{ $post->content }}</p>
+        <h2>Comments</h2><br>
+        @foreach($post->comments as $comment)
+            <b>Nome: </b>{{ $comment->name }}<br>
+            <b>Comment: </b>{{ $comment->comment }}
+        @endforeach
         <hr>
     @endforeach
 
