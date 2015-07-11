b7889c67205adac564a5e58e8a7694ff416a95f5
Valdir Lourenco <valdir2310@yahoo.com.br>
2015-07-11 12:48:30 +0000

    posts tags criado

---
+++ database/migrations/2015_07_11_124354_create_tags_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts_tags', function (Blueprint $table) {
+            $table->increments('post_id');
+            $table->foreign('post_id')=>references('id')->on('posts');
+            $table->integer('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts_tags');
+    }
+}
