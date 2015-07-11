00f4d497231a2678adfcb288ca4f17c47a2d413e
Valdir Lourenco <valdir2310@yahoo.com.br>
2015-07-11 01:53:52 +0000

    Seeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::truncate();
+
+        factory('App\Post', 15)->create();
+    }
+}
